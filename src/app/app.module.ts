98069b26939341f85e5e3d968d733348ccec9d57
Naqeeb4404 <naqeeblat@gmail.com>
2022-12-26 21:44:02 +0800

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
