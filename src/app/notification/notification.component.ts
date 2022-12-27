aed6bbb83c81abf00135a7461e988dfb78102942
Naqeeb4404 <naqeeblat@gmail.com>
2022-12-27 17:37:14 +0800

    angular

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: '<div><p>This website uses cookies to prodive better user experience.</p></div>',
-  styleUrls: ['./notification.component.css']
+  template: `<div class="notification-div">
+    <p>This website "hello" uses cookies to prodive better user experience.</p>
+    </div>`,
+  styles: [".notification-div{margin: 10px 0px; padding: 10px 20px; background-color: #FAD7A0; text-align:center;}", "p{font-size: 14px;}"]
 })
 export class NotificationComponent implements OnInit {
 
