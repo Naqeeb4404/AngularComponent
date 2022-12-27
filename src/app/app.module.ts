05a6136ecc477f4b4f01a5ffb3536825c58af613
Naqeeb4404 <naqeeblat@gmail.com>
2022-12-27 16:12:09 +0800

    amgular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
-    NavComponent
+    HeaderComponent,
+    NavComponent,
+    NotificationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: '<div><p>This website uses cookies to prodive better user experience.</p></div>',
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
