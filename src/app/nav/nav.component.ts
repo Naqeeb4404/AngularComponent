a138af25c837dab65dfd2ba2baf6bbb4a5af4810
Naqeeb4404 <naqeeblat@gmail.com>
2022-12-28 08:46:31 +0800

    angular

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
+  selector: '.app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
