270bfcf4a4fbe1e5e6059fb9211b9ddd5e3c4330
Naqeeb4404 <naqeeblat@gmail.com>
2023-01-03 21:48:53 +0800

    angular

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+searchValue: string ="";
+changeSearchValue(){
+  
+}
 }
