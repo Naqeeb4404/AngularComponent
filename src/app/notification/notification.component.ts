90d7f2d5f12bcfd78b77a8d26b4d622761fe5eaf
Naqeeb4404 <naqeeblat@gmail.com>
2023-01-05 20:27:40 +0800

    anguler

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,10 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div class="alert-success alert-warning" [hidden]="displayNotification">
+  template: `<div class="alert alert-success" [hidden]="displayNotification">
        <p>This website "hello" uses cookies to prodive better user experience.</p>
-       <div class="close"><button class="btn">X</button></div>
-       
+       <div class="close"><button class="btn" (click)="closednotification()">X</button></div>
         </div>`,
     styles: ["div{margin: 10px 0px; padding: 10px 20px; background-color: #FAD7A0; text-align:center;}", "p{font-size: 14px;}", "p{font-size: 14px;}", ".close{float: right; margin-top: -15px;}"]
 })
@@ -17,4 +16,9 @@ export class NotificationComponent implements OnInit {
 
   }
 displayNotification:boolean = true;
+
+closednotification(){
+  this.displayNotification = true;
+}
+
 }
